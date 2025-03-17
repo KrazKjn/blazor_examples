d843565325f69aa7592a3be14b86e119e5b74752
krazkjn <krazkjn@msn.com>
2025-03-17 14:20:12 -0500

    Added App Version and new display page Added functionality to detect local dev or hosted and update base href and background images Added CSS for specific menu Icons

--- PersonalWebsite/Program.cs
+++ PersonalWebsite/Program.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using PersonalWebsite;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+// Continue app initialization
+// Build the service provider
+var host = builder.Build();
+
+var jsRuntime = host.Services.GetRequiredService<IJSRuntime>();
+
+// Call a JavaScript function using IJSRuntime (example)
+await jsRuntime.InvokeVoidAsync("console.log", "Base href set at runtime!");
+
+await host.RunAsync();
